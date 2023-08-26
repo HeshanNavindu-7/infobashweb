9c72c1b4c5ed94476f1cd2b4da54163d73a8c569
sashikalahiru <lahirusashika@gmail.com>
2023-08-26 13:57:31 +0530

    update

--- invite.js
+++ invite.js
@@ -6,7 +6,7 @@ function times1(){
     times+=1;
     console.log(times)
 
-    if(times==1){
+    if(times==13){
         clearInterval(timesCount)
         container.classList.add('v1')
 
@@ -16,3 +16,18 @@ function times1(){
 
 }
 var timesCount=setInterval(times1,1000)
+
+const urlParams = new URLSearchParams(window.location.search);
+        const imageName = urlParams.get('image');
+        const lecturerPosition = urlParams.get('position');
+var invitation=document.querySelector('.invitation')
+var dis=invitation.querySelector('.dis')
+var h2=dis.getElementsByTagName('h2')[0]
+var h3=dis.getElementsByTagName('h3')[0]
+var image=document.getElementById('img')
+
+image.src='./invitations/'+imageName+'.jpg';
+
+
+
+console.log(image.src)
